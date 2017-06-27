8447b3a8135c4759aab3051e89a3ffcd83c45bfa
mekatama <tamatte@hotmail.com>
2017-06-27 19:01:44 +0900

    フタの開閉スクリプト

---
+++ FutaMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FutaMove : MonoBehaviour {
+	GameObject gameController;	//検索したオブジェクト入れる用
+
+	void Start(){
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		TumikiPut gc = gameController.GetComponent<TumikiPut>();
+		//蓋の開閉フラグを見る
+		if(gc.tumikiNagasu == true){
+			transform.position = new Vector2(-5.5f,-2.5f);	//強引にオブシェクトを動かす
+		}else{
+			transform.position = new Vector2(0.0f,-2.5f);	//強引にオブシェクトを動かす
+		}
+	}
+}
