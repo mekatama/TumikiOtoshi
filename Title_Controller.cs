1878f0aa010c732b6eafac5f7d48c1daa35ff414
mekatama <tamatte@hotmail.com>
2017-06-28 12:51:49 +0900

    タイトルに仮のボタンを配置

---
+++ Title_Controller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title_Controller : MonoBehaviour {
+	//スタートボタン用の制御関数
+	public void OnStartButtonClicked(){
+		Application.LoadLevel("main");	//シーンのロード
+	}
+
+	//遊び方ボタン用の制御関数
+	public void OnHowToPlayButtonClicked(){
+		Application.LoadLevel("HowToPlay");	//シーンのロード
+	}
+
+	//タイトルに戻るボタン用の制御関数
+	public void OnReturnTitleButtonClicked(){
+		Application.LoadLevel("Title");	//シーンのロード
+	}
+}
