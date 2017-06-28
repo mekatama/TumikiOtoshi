5057ea840cc2a1f0c42583b38b53ddfb547fb40d
mekatama <tamatte@hotmail.com>
2017-06-28 12:52:30 +0900

    総積木生成数を表示できようにる

---
+++ UI_TumikiScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_TumikiScore : MonoBehaviour {
+
+	public Text scoreLabel;
+//	public GameObject player;
+//	public int total_Score = 0;	//表示される合計スコア
+
+	GameObject gameController;	//検索したオブジェクト入れる用
+
+	void Start(){
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		TumikiPut gc = gameController.GetComponent<TumikiPut>();
+		scoreLabel.text = gc.tumikiNumAll.ToString("000000");
+	}
+}
