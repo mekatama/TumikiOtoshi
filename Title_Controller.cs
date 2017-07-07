7d8b13a408feb6b6eb831a429fceedf411941aa3
mekatama <tamatte@hotmail.com>
2017-07-07 16:50:17 +0900

    アプリ終了機能、ロードシーンのスクリプト修正

--- Title_Controller.cs
+++ Title_Controller.cs
@@ -1,19 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Title_Controller : MonoBehaviour {
+
+	void Update(){
+		//backキー
+		if (Input.GetKeyUp(KeyCode.Escape)){
+			Application.Quit();
+		}
+	}
+
 	//スタートボタン用の制御関数
 	public void OnStartButtonClicked(){
-		Application.LoadLevel("main");	//シーンのロード
+		SceneManager.LoadScene("main");	//シーンのロード
 	}
 
 	//遊び方ボタン用の制御関数
 	public void OnHowToPlayButtonClicked(){
-		Application.LoadLevel("HowToPlay");	//シーンのロード
+		SceneManager.LoadScene("HowToPlay");	//シーンのロード
 	}
 
 	//タイトルに戻るボタン用の制御関数
 	public void OnReturnTitleButtonClicked(){
-		Application.LoadLevel("Title");	//シーンのロード
+		SceneManager.LoadScene("Title");	//シーンのロード
+	}
+
+	//コレクション画面へ行くボタン用の制御関数
+	public void OnCollectionButtonClicked(){
+		SceneManager.LoadScene("Collection");	//シーンのロード
+	}
+
+	//アプリ終了
+	public void OnExit(){
+		Application.Quit();
+		Debug.Log("exit");
 	}
 }
