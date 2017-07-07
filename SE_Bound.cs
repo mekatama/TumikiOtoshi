1e5b98ff2b7735f36011211680500e6d655011e2
mekatama <tamatte@hotmail.com>
2017-07-07 16:50:41 +0900

    下部バウンド時のSE用スクリプト

---
+++ SE_Bound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SE_Bound : MonoBehaviour {
+	AudioSource boundSound;			//AudioSourceコンポーネント取得用
+
+	void Start () {
+		boundSound = GetComponent<AudioSource>();
+	}
+	
+	//他のオブジェクトとの当たり判定
+	void OnCollisionEnter2D(Collision2D other) {
+//		if(other.tag == "Tumiki"){
+			boundSound.Play();	//SE再生
+			Debug.Log("SE再生");
+//		}
+	}
+}
