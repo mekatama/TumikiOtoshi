0c492d20f23a88ebb3f88deabbb0d177234f2877
mekatama <tamatte@hotmail.com>
2017-07-20 16:15:44 +0900

    蓋の開閉時にじんぐるをならす

--- FutaMove.cs
+++ FutaMove.cs
@@ -2,10 +2,16 @@
 using System.Collections;
 
 public class FutaMove : MonoBehaviour {
-	GameObject gameController;	//検索したオブジェクト入れる用
+	GameObject gameController;			//検索したオブジェクト入れる用
+	AudioSource audioSource;			//AudioSourceコンポーネント取得用
+	public AudioClip audioClipOpen;		//開くSE
+	public AudioClip audioClipClose;	//閉じるSE
+
+	private int seGo = 1;				//SEなり分けようフラグ
 
 	void Start(){
 		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+		audioSource = gameObject.GetComponent<AudioSource>();		//AudioSourceコンポーネント取得
 	}
 	
 	void Update () {
@@ -13,9 +19,21 @@ public class FutaMove : MonoBehaviour {
 		TumikiPut gc = gameController.GetComponent<TumikiPut>();
 		//蓋の開閉フラグを見る
 		if(gc.tumikiNagasu == true){
+			//OPEN
 			transform.position = new Vector2(-5.5f,-1.1f);	//強引にオブシェクトを動かす
+			if(seGo == 1){
+				audioSource.clip = audioClipClose;				//SE決定
+				audioSource.Play ();							//SE再生
+				seGo = 0;
+			}
 		}else{
+			//CLOSE
 			transform.position = new Vector2(0.0f,-1.1f);	//強引にオブシェクトを動かす
+			if(seGo == 0){
+				audioSource.clip = audioClipOpen;				//SE決定
+				audioSource.Play ();							//SE再生
+				seGo = 1;
+			}
 		}
 	}
 }
