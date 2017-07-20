8b6c7600ad784be8753c6434064b6af3097ca610
mekatama <tamatte@hotmail.com>
2017-07-20 16:16:17 +0900

    バウンド時のSE再生とモーション

--- SE_Bound.cs
+++ SE_Bound.cs
@@ -5,14 +5,21 @@ public class SE_Bound : MonoBehaviour {
 	AudioSource boundSound;			//AudioSourceコンポーネント取得用
 
 	void Start () {
-		boundSound = GetComponent<AudioSource>();
+		boundSound = GetComponent<AudioSource>();	//AudioSourceコンポーネント取得
 	}
 	
 	//他のオブジェクトとの当たり判定
 	void OnCollisionEnter2D(Collision2D other) {
-//		if(other.tag == "Tumiki"){
-			boundSound.Play();	//SE再生
-			Debug.Log("SE再生");
-//		}
+		boundSound.Play();	//SE再生
+		//ちょっとだけ拡大させる
+		transform.localScale = new Vector2(1.4f, 1.4f);
+		//0.1秒後に呼び出す
+    	Invoke("ScaleReset", 0.07f);
+	}
+
+	//時間差ですけーるを戻したい用
+	void ScaleReset(){
+		//元のスケールに戻す
+		transform.localScale = new Vector2(1.28f, 1.28f);
 	}
 }
