0ef5b1d520b1204a0986d3be6d70c2388078b157
mekatama <tamatte@hotmail.com>
2017-07-20 16:17:22 +0900

    タップ時のEffectとSEを設定

--- TumikiPut.cs
+++ TumikiPut.cs
@@ -5,19 +5,29 @@ public class TumikiPut : MonoBehaviour {
 	public GameObject tumiki_01;	//タップして出現させたいプレハブ
 	public GameObject tumiki_02;	//タップして出現させたいプレハブ
 	public GameObject tumiki_03;	//タップして出現させたいプレハブ
+	public GameObject tumiki_04;	//タップして出現させたいプレハブ
+	public GameObject tumiki_05;	//タップして出現させたいプレハブ
+	public GameObject tumiki_06;	//タップして出現させたいプレハブ
+	public GameObject tumiki_07;	//タップして出現させたいプレハブ
+	public GameObject tumiki_08;	//タップして出現させたいプレハブ
 	public int tumikiNum = 0;		//生成したプレハブの数を保存(フタ開閉用)
 	public int tumikiNumAll = 0;	//生成したプレハブの数を保存(総数)
-	public bool tumikiNagasu = false;	//
+	public bool tumikiNagasu = false;	//つむきを流すかどうかのフラグ
 	AudioSource tapSound;			//AudioSourceコンポーネント取得用
-	ParticleSystem tapEffect;		//
+	ParticleSystem tapEffect;		//タップ時Effect用
 
-	public static bool tumikiOpen1 = false;
+	public static bool tumikiOpen1 = false;	//コレクション画面用のフラク
 	public static bool tumikiOpen2 = false;
 	public static bool tumikiOpen3 = false;
+	public static bool tumikiOpen4 = false;
+	public static bool tumikiOpen5 = false;
+	public static bool tumikiOpen6 = false;
+	public static bool tumikiOpen7 = false;
+	public static bool tumikiOpen8 = false;
 
 	void Start () {
-		tapSound = GetComponent<AudioSource>();
-		tapEffect = GetComponent<ParticleSystem>();
+		tapSound = GetComponent<AudioSource>();		//SE用コンポーネント取得用
+		tapEffect = GetComponent<ParticleSystem>();	//EFFECT用コンポーネント取得用
 	}
 
 	void Update () {
@@ -38,23 +48,39 @@ public class TumikiPut : MonoBehaviour {
 				tapEffect.transform.position = vec;
 				tapEffect.Emit(1);
 
-				int ran = Random.Range(0,3);											//ランダム
-				switch(ran){
-					case 0:
-						Instantiate(tumiki_01,vec,tumiki_01.transform.rotation);			//プレハフ生成
-						tumikiOpen1 = true;
-						tapSound.Play();	//SE再生
-						break;
-					case 1:
-						Instantiate(tumiki_02,vec,tumiki_02.transform.rotation);			//プレハフ生成
-						tumikiOpen2 = true;
-						tapSound.Play();	//SE再生
-						break;
-					case 2:
-						Instantiate(tumiki_03,vec,tumiki_03.transform.rotation);			//プレハフ生成
-						tumikiOpen3 = true;
-						tapSound.Play();	//SE再生
-						break;
+				int ran = Random.Range(0,200);											//ランダム
+				if(ran >= 0 && ran < 61){
+					Instantiate(tumiki_01,vec,tumiki_01.transform.rotation);			//プレハフ生成
+					tumikiOpen1 = true;
+					tapSound.Play();	//SE再生
+				}else if(ran >= 61 && ran < 111){
+					Instantiate(tumiki_02,vec,tumiki_02.transform.rotation);			//プレハフ生成
+					tumikiOpen2 = true;
+					tapSound.Play();	//SE再生
+				}else if(ran >= 111 && ran < 151){
+					Instantiate(tumiki_03,vec,tumiki_03.transform.rotation);			//プレハフ生成
+					tumikiOpen3 = true;
+					tapSound.Play();	//SE再生
+				}else if(ran >= 151 && ran < 188){
+					Instantiate(tumiki_04,vec,tumiki_04.transform.rotation);			//プレハフ生成
+					tumikiOpen4 = true;
+					tapSound.Play();	//SE再生
+				}else if(ran >= 188 && ran < 194){
+					Instantiate(tumiki_05,vec,tumiki_05.transform.rotation);			//プレハフ生成
+					tumikiOpen5 = true;
+					tapSound.Play();	//SE再生
+				}else if(ran >= 194 && ran < 97){
+					Instantiate(tumiki_06,vec,tumiki_06.transform.rotation);			//プレハフ生成
+					tumikiOpen6 = true;
+					tapSound.Play();	//SE再生
+				}else if(ran >= 97 && ran < 199){
+					Instantiate(tumiki_07,vec,tumiki_07.transform.rotation);			//プレハフ生成
+					tumikiOpen7 = true;
+					tapSound.Play();	//SE再生
+				}else if(ran == 199){
+					Instantiate(tumiki_08,vec,tumiki_08.transform.rotation);			//プレハフ生成
+					tumikiOpen8 = true;
+					tapSound.Play();	//SE再生
 				}
 				tumikiNum += 1;
 				tumikiNumAll += 1;
@@ -64,7 +90,7 @@ public class TumikiPut : MonoBehaviour {
 		}
 
 		//積木生成数でフラグのon/off
-		if(tumikiNum > 30 && tumikiNagasu == false){
+		if(tumikiNum == 30 && tumikiNagasu == false){
 			tumikiNagasu = true;
 		}
 		if(tumikiNum == 0 && tumikiNagasu == true){
